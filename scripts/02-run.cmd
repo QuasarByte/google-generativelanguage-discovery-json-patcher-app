d5ba5309d2801f7acf0c5409baf2a0794943e229
roman.taluyev <taluyev@gmail.com>
2024-10-22 16:57:27 -0700

    Added console application

---
+++ scripts/02-run.cmd
@@ -0,0 +1,3 @@
+set path=%path%;c:/opt/java/jdk-21.0.2/bin
+
+java -jar ../target/google-generativelanguage-discovery-json-patcher-app-1.0-SNAPSHOT.jar ../documents/generativelanguage-discovery-all-20241016.json ../documents/generativelanguage-discovery-all-patched-20241016.json
